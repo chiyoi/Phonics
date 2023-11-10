c5323cfd80e10909a6d76007b1e4336604f3cde4
chiyoi <chiyoi2140@outlook.com>
2023-11-10 18:27:47 +0800

    update

--- Phonics/Intents/Diffuse.swift
+++ Phonics/Intents/Diffuse.swift
@@ -6,9 +6,10 @@
 //
 
 import AppIntents
-import Foundation
-import StableDiffusion
 import CoreML
+import Foundation
+
+import StableDiffusion
 
 #if os(macOS)
 import AppKit
@@ -17,29 +18,34 @@ import UIKit
 #elseif os(visionOS)
 #endif
 
-struct Diffuse: AppIntent {
+struct Diffuse: ProgressReportingIntent {
     
-    static var title: LocalizedStringResource = "Diffuse"
-    static var description = IntentDescription("Diffuse with prompt.")
+    static let title: LocalizedStringResource = "Diffuse"
+    static let description = IntentDescription("Diffuse with prompt.")
     
     static var parameterSummary: some ParameterSummary {
-        Summary("Diffuse with \(\.$prompt)")
+        Summary("Diffuse with \(\.$prompt)") {
+            \.$stepCount
+            \.$seed
+        }
     }
     
     @Parameter(
         title: "Prompt",
-        description: "Prompt to diffuse."
+        description: "(Text) Prompt to diffuse."
     ) var prompt: String
     
     @Parameter(
         title: "Step Count",
-        description: "Diffusion steps.",
-        default: 30
+        description: "(Integer) Diffusion steps. (optional)",
+        default: 30,
+        controlStyle: .stepper
     ) var stepCount: Int
     
     @Parameter(
         title: "Seed",
-        description: "Random seed."
+        description: "(Integer) Random seed. (optional)",
+        controlStyle: .field
     ) var seed: Int?
     
     static var resourceURL: URL? {
@@ -51,12 +57,44 @@ struct Diffuse: AppIntent {
     
     @MainActor
     func perform() async throws -> some IntentResult & ReturnsValue<IntentFile> {
-        let config = MLModelConfiguration()
-        config.computeUnits = .cpuAndNeuralEngine
+        print("Diffuse v0.1.6")
         
+        let seed = seed != nil ? UInt32(seed!) : UInt32.random(in: 0...UInt32.max)
+        let targetSize: Float = 512
+        
+        print("Generate image.")
+        let image = try await Self.diffuse(
+            with: prompt,
+            stepCount: stepCount,
+            seed: seed,
+            targetSize: targetSize
+        ) { pipelineProgress in
+            if progress.isIndeterminate {
+                progress.totalUnitCount = Int64(stepCount)
+            }
+            progress.completedUnitCount = Int64(pipelineProgress.step)
+            await Task.yield()
+            return true
+        }
+        
+        let data = try Self.cgImageData(cgImage: image)
+        let filename = "\(prompt)-\(seed).png"
+        return .result(value: IntentFile(data: data, filename: filename, type: .png))
+    }
+    
+    static func diffuse(
+        with prompt: String,
+        stepCount: Int,
+        seed: UInt32,
+        targetSize: Float,
+        progressHandler: (PipelineProgress) async -> Bool
+    ) async throws -> CGImage {
         guard let resourceURL = Self.resourceURL else {
             throw Error.failedLoadingResource
         }
+        
+        let config = MLModelConfiguration()
+        config.computeUnits = .cpuAndNeuralEngine
         
         let pipeline = try StableDiffusionPipeline(
             resourcesAt: resourceURL,
@@ -70,33 +108,38 @@ struct Diffuse: AppIntent {
         
         try pipeline.loadResources()
         
-        let seed = seed != nil ? UInt32(seed!) : UInt32.random(in: 0...UInt32.max)
-        let targetSize: Float = 512
-        
         var pipelineConfig = StableDiffusionPipeline.Configuration(prompt: prompt)
         pipelineConfig.stepCount = stepCount
         pipelineConfig.seed = seed
         pipelineConfig.targetSize = targetSize
         
-        let images = try pipeline.generateImages(configuration: pipelineConfig)
+        let images = try await pipeline.generateImages(
+            configuration: pipelineConfig,
+            progressHandler: progressHandler
+        )
         
         guard let image = images[0] else {
             throw Error.failedGettingResult
         }
-        
+        return image
+    }
+    
+    static func cgImageData(cgImage: CGImage) throws -> Data  {
 #if os(macOS)
-        let bitmap = NSBitmapImageRep(cgImage: image)
+        let bitmap = NSBitmapImageRep(cgImage: cgImage)
         guard let data = bitmap.representation(using: .png, properties: [:]) else {
             throw Error.failedGettingResult
         }
 #elseif os(iOS)
+        let uiImage = UIImage(cgImage: cgImage)
+        guard let data = uiImage.pngData() else {
+            throw Error.failedGettingResult
+        }
 #endif
-        
-        let filename = "\(prompt)-\(seed).png"
-        return .result(value: IntentFile(data: data, filename: filename, type: .png))
+        return data
     }
     
     enum Error: Swift.Error {
-        case failedLoadingResource, failedGettingResult
+        case failedLoadingResource, failedRunningDiffuse, failedGettingResult
     }
 }
